c349051911459895d01f0ca5547e90609d7b5ad2
Vlad Oncioiu <ovlad@users.noreply.github.com>
2017-04-22 11:51:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 apt-get update
-apt-get install python2.7 python-pip python-django python-dev python-virtualenv libldap2-dev libsasl2-dev libssl-dev
+apt-get install git python2.7 python-pip python-django python-dev python-virtualenv libldap2-dev libsasl2-dev libssl-dev
+git clone https://github.com/rosedu/wouso
+cd wouso 
+pip install -r requirements-pip
